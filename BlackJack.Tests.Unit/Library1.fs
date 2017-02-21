9a10c8746697259dcf0270ad868a390b8088a4f8
Richard Gibson <richard.gibson@asos.com>
2017-02-21 14:32:02 +0000

    Some basic domain types and functions and a taster test

---
+++ BlackJack.Tests.Unit/Library1.fs
@@ -0,0 +1,20 @@
+﻿module BlackJack.Tests.Unit.DomainTests
+
+open BlackJack.Domain
+open Xunit
+open Swensen.Unquote
+
+[<Fact>]
+let ``Score card returns correct int value`` () =
+    let card = Hearts, Two
+    let actual = scoreCard card
+
+    test <@ actual = 2 @>
+
+(*
+When I am dealt a Queen and a King
+And the dealer is dealt a pair of Kings
+And I stick
+And the dealer sticks
+Then the result should be a draw
+ *)
---
+++ BlackJack/Domain.fs
@@ -0,0 +1,41 @@
+﻿module BlackJack.Domain
+
+type Suit = Hearts | Clubs | Diamonds | Spades
+
+type Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
+
+type Card = Suit * Rank
+
+type Player = Player of string
+
+type Hand = Card list
+
+type PlayerHand = Player * Hand
+
+type DealerHand = Dealer of Hand
+
+type MoveType = Stick | Flip
+
+type GameState = DealerHand * (PlayerHand list)
+
+
+
+let notImplemented () = raise (new System.NotImplementedException())
+
+let playerPlay moveType player gameState = notImplemented ()
+
+let dealerPlay gameState = notImplemented ()
+
+let scoreCard (card : Card) =
+    match snd card with
+    | Two                             -> 2
+    | Three                           -> 3
+    | Four                            -> 4
+    | Five                            -> 5
+    | Six                             -> 6
+    | Seven                           -> 7
+    | Eight                           -> 8
+    | Nine                            -> 9
+    | Ten | Jack | Queen | King | Ace -> 10
+
+let scoreHand hand = hand |> Seq.map (scoreCard) |> Seq.fold (+) 0
